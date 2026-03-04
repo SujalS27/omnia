b04b2115e2f619d20c601e00b99ada619b1be2bb
SujalS27 <Sujal.Sooryavamshi@dell.com>
2026-03-04 14:38:21 +0530

    deleted unwanted fix_oim_timezone file that I had created

--- fix_oim_timezone.ps1
+++
@@ -1,9 +0,0 @@
-# Script to fix oim_timezone in all template files
-Get-ChildItem "discovery\roles\openchami_cloud_init\templates\cloud_init\*.yaml.j2" | ForEach-Object {
-    $content = Get-Content $_.FullName
-    if ($content -match "hostvars\['oim'\]\['oim_timezone'\]") {
-        $content = $content -replace "hostvars\['oim'\]\['oim_timezone'\]", "hostvars['oim']['oim_timezone'] | default('UTC')"
-        Set-Content $_.FullName $content
-        Write-Host "Fixed $($_.Name)"
-    }
-}
